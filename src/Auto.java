01746bda603ad4a1dedc5353c2a1a8ae69f3dda2
Alessandro <sandroscro@libero.it>
2023-10-15 17:23:06 +0200

    Updated Main and Auto

---
+++ src/Auto.java
@@ -0,0 +1,49 @@
+//Scrivere un programma che contenga una classe che definisce un Auto che abbia come proprietà cilindrata,
+// targa, marca e modello.
+//Per questa classe saranno necessari i metodi accessori (getters e setters) ed un costruttore.
+//Nel main dichiarare un oggetto di tipo Auto.
+public class Auto {
+    private float cilindrata;
+    private String targa;
+    private String marca;
+    private String modello;
+
+    public Auto(float cilindrata, String targa, String marca, String modello) {
+        this.cilindrata = cilindrata;
+        this.targa = targa;
+        this.marca = marca;
+        this.modello = modello;
+    }
+
+    public float getCilindrata() {
+        return cilindrata;
+    }
+
+    public String getTarga() {
+        return targa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModello() {
+        return modello;
+    }
+
+    public void setCilindrata(float cilindrata) {
+        this.cilindrata = cilindrata;
+    }
+
+    public void setTarga(String targa) {
+        this.targa = targa;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModello(String modello) {
+        this.modello = modello;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+    public void main(String[] args){
+        Auto auto1=new Auto((float) 1.4F, "AZ999ZA","HYUNDAI", "TUCSON");
+        auto1.setCilindrata(1.6F);
+
+        System.out.println(auto1.getCilindrata());
+
+    }
+}
