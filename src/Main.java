4620dd68d2764fb00b435dbf9eb15f51300cffd6
Alessandro <sandroscro@libero.it>
2023-10-15 17:26:08 +0200

    Corrected Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main{
-    public void main(String[] args){
+    public static void main(String[] args){
         Auto auto1=new Auto((float) 1.4F, "AZ999ZA","HYUNDAI", "TUCSON");
         auto1.setCilindrata(1.6F);
 
