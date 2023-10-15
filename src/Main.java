da437e1760ee33ea535922a6b175c4205a8b1156
Alessandro <sandroscro@libero.it>
2023-10-15 17:30:27 +0200

    Changed auto1.getCilindrata with auto1

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main{
         Auto auto1=new Auto((float) 1.4F, "AZ999ZA","HYUNDAI", "TUCSON");
         auto1.setCilindrata(1.6F);
 
-        System.out.println(auto1.getCilindrata());
+        System.out.println(auto1);
 
     }
 }
